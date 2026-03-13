4ccabb09f146eab75705206c28099bcaf5889636
istrainovic <ivan.strainovic@gmail.com>
2026-03-13 09:40:52 +0100

    export new domain models in domain package

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -2,5 +2,9 @@
 
 from .product import Product
 from .warehouse import Warehouse
+from .member import Member
+from .employee import Employee
+from .equipment import Equipment
+from .vending_machine import VendingMachine
 
 __all__ = ["Product", "Warehouse"]
