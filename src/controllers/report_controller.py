c7671094ddf8b2ebb498dde7f2b4c768b5cde6cc
istrainovic <ivan.strainovic@gmail.com>
2026-04-10 08:27:16 +0200

    feat(controller): add report controller

---
+++ src/controllers/report_controller.py
@@ -0,0 +1,6 @@
+def generate_inventory_report(service):
+    return service.generate_inventory_report()
+
+
+def generate_equipment_status_report(service):
+    return service.generate_equipment_status_report()
